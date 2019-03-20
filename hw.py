ac6b0c94fe0a913975853309ab8b9de50b8b91b9
Behruz1 <48621763+Behruz1@users.noreply.github.com>
2019-03-20 18:44:02 +0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''Question 1'''
+#l=[]
+#for n in range(2000,3201):
+#    if n%7==0 and n%5!=0:
+#        l.append(str(n))
+#print (','.join(l))
+
+
+'''Question 2'''
+#b=1
+#a=int(input("Enter any number "))
+#for n in range (1,a+1):
+#    b=b*n 
+#print(b)
+
+
+'''Question 3'''
+d=list(input("Enter the list of numbers "))
+D=d
+print(D)
